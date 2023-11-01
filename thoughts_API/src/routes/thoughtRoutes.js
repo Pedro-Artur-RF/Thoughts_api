f1b7804c1f7f70cc02ae40f7e0a705eda4a8ab9f
Pedro Artur <71746213+Pedro-Artur-RF@users.noreply.github.com>
2023-11-01 11:09:34 -0300

    Delete thoughts_API directory

--- thoughts_API/src/routes/thoughtRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const ThoughtsController = require("../controllers/ThoughtsController");
-const UsersController = require("../controllers/UsersController");
-const router = express.Router();
-
-router.post("/users", UsersController.createUser);
-router.get("/thoughts", ThoughtsController.findAllThoughts);
